3554803960994267d0e9ad05a30372bc012d86b2
wiless <wiless@users.noreply.github.com>
2020-11-26 12:26:44 +0530

    Update api.js

--- static/api.js
+++ static/api.js
@@ -10,7 +10,7 @@ function API() {
   let funcpaths = new Map();
 
   (function () {        
-    apiurl = "https://wilessapi.appspot.com";
+    apiurl = "https://sendildevar.appspot.com";
     funcpaths.set("new", { path: "/new", method: "get" })
     .set("system", { path: "/system", method: "get" })
     .set("login", { path: "/login", method: "post" })
