13b5daee0e253cdc5e82d8404429cebbc941b1c5
wireless <wiless@users.noreply.github.com>
2021-07-21 17:39:31 +0530

    Update service.js

--- _apps/service.js
+++ _apps/service.js
@@ -16,13 +16,13 @@ console.log('service.js: Hello from service worker')
 // })
 
 self.addEventListener('push', function (event) {
-      self.clients.matchAll({type:"window"}).then(function(clientList) {    
-       console.log("Found these clients ",clientList)
-       }
+      
+      self.clients.matchAll({type:"window"})
+            .then(clientList=>console.log("Found these clients ",clientList));
                                                   
     if (event.data) {
         console.log('Push event!! ', event.data.text())
-//         showLocalNotification("Message", event.data.text(), self.registration);
+        //showLocalNotification("Message", event.data.text(), self.registration);
         self.registration.showNotification("Title : Message", {body:event.data.text()});
     } else {
         console.log('Push event but no data')
