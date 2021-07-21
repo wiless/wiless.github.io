5ad1c48ac72fcef82b3556e63bd774e9e99ca8b7
wireless <wiless@users.noreply.github.com>
2021-07-21 16:34:55 +0530

    Update service.js

--- _apps/service.js
+++ _apps/service.js
@@ -16,38 +16,38 @@ console.log('service.js: Hello from service worker')
 // })
 
 
-self.addEventListener('push', function (event) {
-    if (event.data) {
-        console.log('Push event!! ', event.data.text())
-        showLocalNotification("Message", event.data.text(), self.registration);
-    } else {
-        console.log('Push event but no data')
-    }
-})
+// self.addEventListener('push', function (event) {
+//     if (event.data) {
+//         console.log('Push event!! ', event.data.text())
+//         showLocalNotification("Message", event.data.text(), self.registration);
+//     } else {
+//         console.log('Push event but no data')
+//     }
+// })
 
 
 
 
-const showLocalNotification = (title, body, swRegistration) => {
-    // actions: [{ action: "button", title: "<" }, { action: "right", title: ">" }],
-    self.clients.matchAll({type:"window"}).then(function(clientList) {        
+// const showLocalNotification = (title, body, swRegistration) => {
+//     // actions: [{ action: "button", title: "<" }, { action: "right", title: ">" }],
+//     self.clients.matchAll({type:"window"}).then(function(clientList) {        
         
-  // do something with your clients list
-         for (var i = 0; i < clientList.length; i++) {
-      var client = clientList[i];
-      if (client.url == '/' && 'focus' in client) {
-        client.focus();
-        break;
-      }
-    }
+//   // do something with your clients list
+//          for (var i = 0; i < clientList.length; i++) {
+//       var client = clientList[i];
+//       if (client.url == '/' && 'focus' in client) {
+//         client.focus();
+//         break;
+//       }
+//     }
         
         
-});
+// });
     
-    const options = {
-        body: body,
-        // here you can add more properties like icon, image, vibrate, etc.
-    };
-    swRegistration.showNotification(title, options);
+//     const options = {
+//         body: body,
+//         // here you can add more properties like icon, image, vibrate, etc.
+//     };
+//     swRegistration.showNotification(title, options);
 
-}
+// }
