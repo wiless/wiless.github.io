7abb7bcb74cf0bbe7362280d7793c8b85f615c56
wireless <wiless@users.noreply.github.com>
2021-07-23 10:11:08 +0530

    Update service.js

--- _apps/service.js
+++ _apps/service.js
@@ -24,6 +24,20 @@ self.addEventListener('message', (event) => {
 });
 
 
+let getVersionPort;
+let count = 0;
+self.addEventListener("message", event => {
+  if (event.data && event.data.type === 'INIT_PORT') {
+    getVersionPort = event.ports[0];
+  }
+
+  if (event.data && event.data.type === 'INCREASE_COUNT') {
+    getVersionPort.postMessage({ payload: ++count });
+  }
+}
+
+
+
 self.addEventListener('notificationclick', function(event) {
   console.log('[Service Worker] Notification click Received.');
  console.log('[Service Worker] ',event);
