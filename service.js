4a9778268b87a85012688ef562d6d74b82f1f9e2
wireless <wiless@users.noreply.github.com>
2021-07-25 16:02:38 +0530

    Update service.js

--- service.js
+++ service.js
@@ -57,13 +57,14 @@ self.addEventListener('notificationclick', function(event) {
 
  
 self.addEventListener('push', pushEvent => {   
-  console.log("Inside .. service.js : push listener");
+  console.log("Inside .. service.js : push listener",pushEvent.data);
    if (pushEvent.data) {
      count++;
        var data=pushEvent.data;
        var obj={count:count,data:data};     
         console.log('Service Worker :PUSH Received and Broadcasting now : ', obj);   
         var bcpush = new BroadcastChannel('counterupdates');
+        console.log("Broadcast channel exists ?? ",bcpush);
         bcpush.postMessage({count:count,data:data});       
 //     showLocalNotification("Message", event.data.text(), self.registration);
         pushEvent.waitUntil(self.registration.showNotification("Title : Message", { body: pushEvent.data.text() }));
